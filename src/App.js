c75e87a4bd28dbad55811c4c726fc993dc44c95c
Oscar <oscar.zajac99@gmail.com>
2022-12-05 17:50:58 +0100

    added 2 components and cleaned original create-react-app files a little bit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Data from "./Data";
+import Header from "./Components/Header";
+import Card from "./Components/Card";
 
 function App() {
+  const cards = Data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {cards}
     </div>
   );
 }
